6088a8d4d727b073f297ea0abf03fac7e46491bf
salman-philips <mohamedsalman.siddiq@philips.com>
2021-05-02 16:11:52 +0530

    add the listener and remove it

--- app/src/main/java/com/example/firebase1/MainActivity.java
+++ app/src/main/java/com/example/firebase1/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText firebaseEditText1;
     private TextView readFirebaseTextView1;
     private Button sendToFirebaseButton, readFromFirebaseButton;
+    private ValueEventListener valueEventListener;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
     private String pathForEntry;
@@ -58,13 +59,51 @@ public class MainActivity extends AppCompatActivity {
         // either generate if that particular path doesn't exist in the database for the reference
         // when data is set using set value Or vice versa this would be useful for specifying the part at getting the database reference itself
         pathForEntry = "";
+        valueEventListener=new ValueEventListener() { //bcox such method would be called once over evry initialization or button click
+            //without being called from database change as a part of initialization and returns null for above scanario
+//Simply attaching a  child to a reference which does not exist in the real database and if you try to have valuable listener on it it will be of no use
+// at that age it isn't create new path on the database
+            //Called at the initial initialization of the value event listener wherever it
+            // is initialized or if it is initialized inside some invoke method then it would be called only when such method would be invoked
+            //This method will be invoked every time there is a change in data or a new data it is inserted add the reference node in the database
+            // practice one time it's initial initialization and then every time the data is changed
+//databaseReference.child("sel").addValueEventListener(new ValueEventListener() not possible if such ode dosent exists it will not create any new node sel
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String data = snapshot.getValue(String.class);
+                //However specifying random parts that may or may not exist will cause some exception while reading the data from the
+                // database if such situations of sub directories and child nodes are not handled
+                if (!data.isEmpty()) {
+                    readFirebaseTextView1.setText(data);
+                    Log.d("Sel",data);
+                    //for every press of readData press like for every press it reads data and for 3 presses u have 3 listeners attached on every new update on database
+                    //3 times update on 3 different listeners will be called but when observed 2 times called but when u d
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        databaseReference.child("selman").addValueEventListener(valueEventListener);
     }
+
 
     private void initViews() {
         firebaseEditText1 = findViewById(R.id.FirebaseEditText1);
         sendToFirebaseButton = findViewById(R.id.SendToFirebaseButton);
         readFirebaseTextView1 = findViewById(R.id.FirebaseTextView1);
         readFromFirebaseButton = findViewById(R.id.ReadFirebaseButton);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        databaseReference.child("selman").removeEventListener(valueEventListener);//if u have seperate listeners attached to seperate nodes
+        //u need to explicitly remove them even though the childs listners is removed
     }
 
     public void sendDataToFirebase(View view) {
@@ -78,33 +117,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void readDataFromFirebase(View view) {
-        //Called at the initial initialization of the value event listener wherever it
-        // is initialized or if it is initialized inside some invoke method then it would be called only when such method would be invoked
-        //This method will be invoked every time there is a change in data or a new data it is inserted add the reference node in the database
-        // practice one time it's initial initialization and then every time the data is changed
-//databaseReference.child("sel").addValueEventListener(new ValueEventListener() not possible if such ode dosent exists it will not create any new node sel
-        databaseReference.child("selman").addValueEventListener(new ValueEventListener() { //bcox such method would be called once over evry initialization or button click
-            //without being called from database change as a part of initialization and returns null for above scanario
-//Simply attaching a  child to a reference which does not exist in the real database and if you try to have valuable listener on it it will be of no use
-// at that age it isn't create new path on the database
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String data = snapshot.getValue(String.class);
-                //However specifying random parts that may or may not exist will cause some exception while reading the data from the
-                // database if such situations of sub directories and child nodes are not handled
-                if (!data.isEmpty()) {
-                    readFirebaseTextView1.setText(data);
-                    Log.d("Sel",data);
-                    //for every press of readData press like for every press it reads data and for 3 presses u have 3 listeners attached on every new update on database
-                    //3 times update on 3 different listeners will be called but when observed 2 times called
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+     // databaseReference.child("selman").addValueEventListener(valueEventListener); same as previous not a good practise so add during initFirebaseConfig and
+        // remove on Destroy
     }
 }
